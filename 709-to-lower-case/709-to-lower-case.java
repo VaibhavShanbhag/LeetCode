7aa08b459dfaae2d988d734d60bbdaf127f0648f
Vaibhav Shanbhag <87072667+VaibhavShanbhag@users.noreply.github.com>
2022-06-30 11:47:40 +0530

    Time: 16 ms (5.19%), Space: 42.9 MB (5.64%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans = "";
+        
+        for(char ch: s.toCharArray()){
+            if(Character.isUpperCase(ch)){
+                ans += (char)(ch + 32);
+            }
+            
+            else
+                ans += ch;
+        }
+        
+        return ans;
+    }
+}
